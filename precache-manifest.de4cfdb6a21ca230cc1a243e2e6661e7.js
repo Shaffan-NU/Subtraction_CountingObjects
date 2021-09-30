3fc1b390a1fb11771c170fb684170d28b7c76755
shaffanFast <k190863@nu.edu.pk>
2021-09-30 12:14:49 +0500

    Updates

---
+++ precache-manifest.de4cfdb6a21ca230cc1a243e2e6661e7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6b466a301a936bbcd83",
+    "url": "/Subtraction_CountingObjects/static/css/main.ef7d6e46.chunk.css"
+  },
+  {
+    "revision": "b6b466a301a936bbcd83",
+    "url": "/Subtraction_CountingObjects/static/js/main.ece3d54a.chunk.js"
+  },
+  {
+    "revision": "070b69d6f07f1f06949f",
+    "url": "/Subtraction_CountingObjects/static/js/runtime~main.cef2c381.js"
+  },
+  {
+    "revision": "d12ae2fc5db0fb6d6669",
+    "url": "/Subtraction_CountingObjects/static/css/2.979351be.chunk.css"
+  },
+  {
+    "revision": "d12ae2fc5db0fb6d6669",
+    "url": "/Subtraction_CountingObjects/static/js/2.f7c563fc.chunk.js"
+  },
+  {
+    "revision": "ef3ab8618fdfa8f8c32b",
+    "url": "/Subtraction_CountingObjects/static/js/3.ff72ea0d.chunk.js"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/Subtraction_CountingObjects/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/Subtraction_CountingObjects/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/Subtraction_CountingObjects/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/Subtraction_CountingObjects/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/Subtraction_CountingObjects/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/Subtraction_CountingObjects/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/Subtraction_CountingObjects/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/Subtraction_CountingObjects/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/Subtraction_CountingObjects/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/Subtraction_CountingObjects/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/Subtraction_CountingObjects/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "7890a5da3912ba0d306d379bc66c77f3",
+    "url": "/Subtraction_CountingObjects/static/media/rooster.7890a5da.png"
+  },
+  {
+    "revision": "c040d63c530ff46d273f95822ecb12e7",
+    "url": "/Subtraction_CountingObjects/static/media/football10.c040d63c.png"
+  },
+  {
+    "revision": "c989b2046b4fa39573d918b056723605",
+    "url": "/Subtraction_CountingObjects/static/media/football1.c989b204.png"
+  },
+  {
+    "revision": "1d582e329e33f3fcbe7a4c1f6bea4b75",
+    "url": "/Subtraction_CountingObjects/static/media/roosterSound.1d582e32.wav"
+  },
+  {
+    "revision": "612faa13f56ad11ff00e79ec305555fb",
+    "url": "/Subtraction_CountingObjects/static/media/background.612faa13.gif"
+  },
+  {
+    "revision": "cabc22c6e80bc043c10b098b6b8854bb",
+    "url": "/Subtraction_CountingObjects/static/media/bowl.cabc22c6.png"
+  },
+  {
+    "revision": "10ae2aaca9eb8d6e82dcf0287f6101c0",
+    "url": "/Subtraction_CountingObjects/index.html"
+  }
+];
